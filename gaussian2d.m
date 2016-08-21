e54c48595f096e14724773e71b3d4c49248dab8d
Bingchen Liu <bruce.bc.liu@outlook.com>
2016-08-21 17:03:12 -0400

    Delete gaussian2d.m

--- gaussian2d.m
+++
@@ -1,6 +0,0 @@
-function [filter] = gaussian2d(N,sigma)
-  % N is grid size, sigma speaks for itself
-    [x, y] = meshgrid(round(-N/2):round(N/2), round(-N/2):round(N/2));
-    f = exp(-x.^2/(2*sigma^2)-y.^2/(2*sigma^2));
-    filter = f / sum(f(:));
-end
